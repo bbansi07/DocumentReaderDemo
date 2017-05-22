c652496e7ac369240d51e5593a67d0c9fad56a6e
Bansi Bhatt <bannsi.bhatt07@gmail.com>
2017-05-22 16:33:49 +0530

    first commit

--- DocumentReaderDemo/AppDelegate.swift
+++ DocumentReaderDemo/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  DocumentReaderDemo
 //
-//  Created by Zeitech Solutions on 19/05/17.
+//  Created by Bansi Bhatt on 19/05/17.
 //
 //
 
@@ -41,6 +41,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        return true
+    }
 }
 
--- DocumentReaderDemo/ViewController.swift
+++ DocumentReaderDemo/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  UIActivityDemo
 //
-//  Created by Bansi on 18/05/17.
+//  Created by Bansi Bhatt on 18/05/17.
 //
 //
 
@@ -11,12 +11,16 @@ import QuickLook
 
 class ViewController: UIViewController,QLPreviewControllerDataSource,QLPreviewControllerDelegate,UIDocumentInteractionControllerDelegate{
     var documentInteractionController : UIDocumentInteractionController!
-    let fileURL = "file:///Users/zeitechsolutions/Library/Developer/CoreSimulator/Devices/B6050867-6619-4651-B9FA-16B50FBA8DD3/data/Containers/Data/Application/EB4F8880-3742-4E4A-9E30-B51ECEF7F9A5/Library/Application%20Support/userFiles/2aqlk/jobjrq/jsz8rm/address(1).pdf"
+    
+    let mainbundle = Bundle.main
+    let url = Bundle.main.path(forResource: "mypdf", ofType: "pdf")!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-     let button = UIButton()
-    button.setTitle("name", for: UIControlState.normal)
-        print(button.titleLabel?.text ?? "")
+       documentInteractionController = UIDocumentInteractionController()
+        documentInteractionController.delegate = self
     
     }
 
@@ -24,32 +28,52 @@ class ViewController: UIViewController,QLPreviewControllerDataSource,QLPreviewCo
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-//MARK:- QLPreviewController
+ 
+    //MARK:- QLPreviewController
     @IBAction func buttontapped (sender: UIButton)  {
     
         let viewPDF = QLPreviewController()
+       
         viewPDF.dataSource = self
         viewPDF.delegate = self
-        self.present(viewPDF, animated: true, completion: nil)
+       // self.accessibilityValue = url
+       // self.accessibilityElementsHidden = false
+        
+        //Wants to add this button as share button on navigation Item
+    /*
+    let shareBarButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.action, target: self, action:#selector(openShareActionSheet(_:)))
+   
+    viewPDF.navigationItem.setRightBarButton(shareBarButton, animated: true)
+    */
+          self.present(viewPDF, animated: true, completion: nil)
         
     }
 
     
+    
     func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
-        //let path = Bundle.main.path(forResource: "address", ofType: "pdf")
+        print("Getting item")
+        let mainbundle = Bundle.main
+        let url = mainbundle.path(forResource: "mypdf", ofType: "pdf")!
+        print(url)
+        let doc = URL(fileURLWithPath: url)
         
-       
+        return doc as QLPreviewItem
         
+        //directly open file
+       /*
+        let fileURL = "file:///Users/zeitechsolutions/Library/Developer/CoreSimulator/Devices/B6050867-6619-4651-B9FA-16B50FBA8DD3/data/Containers/Data/Application/EB4F8880-3742-4E4A-9E30-B51ECEF7F9A5/Library/Application%20Support/userFiles/2aqlk/jobjrq/jsz8rm/address(1).pdf"
         let url = NSURL.fileURL(withPath: fileURL)
         
         return url as QLPreviewItem
+ */
     }
     
     func numberOfPreviewItems(in controller: QLPreviewController) -> Int{
         return 1
     }
     
-    
+    //This is not called
     //MARK:- UIDocumentInteractionController
     @IBAction func openUsingDocumentReder(_ sender : UIButton){
         documentInteractionController = UIDocumentInteractionController()
@@ -69,10 +93,15 @@ class ViewController: UIViewController,QLPreviewControllerDataSource,QLPreviewCo
       //  self.present(viewPDF, animated: true, completion: nil)
 
     }
+    
+    func previewController(_ controller: QLPreviewController, shouldOpen url: URL, for item: QLPreviewItem) -> Bool{
+        return true
+    }
+    
     // MARK: UIDocument Interaction Controller
     func openShareActionSheet(_ sender: UIBarButtonItem) {
        // let url = NSURL.fileURL(withPath: fileURL)
-       documentInteractionController = UIDocumentInteractionController(url: URL(fileURLWithPath: fileURL))
+       documentInteractionController = UIDocumentInteractionController(url: URL(fileURLWithPath: url))
         self.documentInteractionController.delegate = self
         self.documentInteractionController.presentOpenInMenu(from: sender, animated: true)
     }
